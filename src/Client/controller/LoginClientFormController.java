ec30aee1848d635f306978c10634efffa833b54e
Kavishka <kavishkaprabath21@gmail.com>
2022-08-19 02:41:18 +0530

    add loginClientForm and loginClientFormController

---
+++ src/Client/controller/LoginClientFormController.java
@@ -0,0 +1,26 @@
+package Client.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @author : Kavishka Prabath
+ * @since : 0.1.0
+ **/
+
+public class LoginClientFormController {
+
+    public AnchorPane root;
+    public JFXButton btnLogin;
+    public TextField txtUsername;
+
+    public void loginOnAction(KeyEvent keyEvent) {
+    }
+
+    public void loginToChatRoomOnAction(ActionEvent actionEvent) {
+    }
+}
